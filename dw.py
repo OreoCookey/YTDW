91b7ff54638914146b6f787ac161823322afe2a4
Eugene Levinson <49786718+OreoCookey@users.noreply.github.com>
2020-09-25 23:27:01 +0100

    v1

--- dw.py
+++ dw.py
@@ -1,17 +1,26 @@
 import yaml
 import time
 import sys
-from pytube import YouTube
+from mypytube import YouTube
 import os 
 from console_progressbar import ProgressBar
-
-#disctionary values
 
 #function to log errors
 def log(err):
     error_file = open("error_logs.txt" , "a")
     error_file.write("\n" + str(err))
     error_file.close()
+
+
+#converting seconds to hours and minutes
+def convert(seconds): 
+    seconds = seconds % (24 * 3600) 
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%dh %02dm %02ds" % (hour, minutes, seconds) 
 
 #try reading the config file
 try:
@@ -84,6 +93,8 @@ if len(urls) > len(names):
 n_to_download = len(urls)
 downloaded = 0
 errors = []
+s_downloaded = 0
+failed = 0
 
 #save directory
 save_dirictory = settings[2]
@@ -93,6 +104,7 @@ save_dirictory = os.path.join(save_dirictory, "YouTubeDw")
 progress = ProgressBar(total=100 ,prefix='Downloading', suffix='Completed', decimals=2, length=50, fill='#', zfill='-')
 
 
+t0 = time.time()
 
 #download all music
 for i in urls:
@@ -100,24 +112,50 @@ for i in urls:
     try:
         audio = YouTube(i).streams.filter(only_audio=True).order_by('bitrate').desc().first()
         audio.download(save_dirictory, names[index])
+        s_downloaded = s_downloaded + 1
     except Exception as err:
-        log(err)
+        log(str(err) + " : " + str(i))
         errors.append(i)
+        failed = failed + 1
 
     downloaded = downloaded + 1
 
     #update progress bar
     progress.print_progress_bar(round((downloaded/n_to_download)*100, 2))
     
+t1 = time.time()
+time_elapsed = t1 - t0
+time_elapsed = round(time_elapsed, 0)
+time_elapsed = convert(time_elapsed)
 
 
-#if any failed to download display which one failed
+print("Downloaded " + str(s_downloaded) + " tracks")
+print("Failed to download " + str(failed) + " tracks")
+print("Total time: " + str(time_elapsed))
+print("Take a look at 'download_report.txt' the stats")
+print("Will quit in 10s")
+
+
+report_msg = "Downloaded " + str(s_downloaded) + " tracks\n"
+report_msg = report_msg + "Failed to download " + str(failed) + " tracks\n"
+report_msg = report_msg + "Total time: " + str(time_elapsed) + "\n"
+report_msg = report_msg + "Failed to download:\n"
+
 if len(errors) != 0:
-    print("Couldn't download the folowing:")
     for i in errors:
-        print(i)
+        report_msg = report_msg + str(i) + "\n"
 
-    print("Check 'error_logs.txt' for more info")
+else:
+    report_msg = report_msg + "None"
+
+
+report = open("download_report.txt", "w+")
+report.write(report_msg)
+report.close()
+time.sleep(10)
+
+
+
 
 
 
